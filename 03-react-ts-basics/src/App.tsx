e4a705f8f8286bf76c057de75a26c534ef409270
I-Maged <magedhanafy@gmail.com>
2026-03-07 21:33:22 +0200

    props and children

---
+++ 03-react-ts-basics/src/App.tsx
@@ -0,0 +1,13 @@
+import CourseGoal from './components/CourseGoal'
+
+const App = () => {
+  return (
+    <main>
+      <CourseGoal title='Learn React + TS'>
+        <p>Learn from the ground up</p>
+      </CourseGoal>
+    </main>
+  )
+}
+
+export default App
