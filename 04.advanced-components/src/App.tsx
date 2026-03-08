b583d0d137b37f1004d0d4d463ca3222227aee01
I-Maged <magedhanafy@gmail.com>
2026-03-09 00:21:34 +0200

    Polymorphic Component

--- 04.advanced-components/src/App.tsx
+++ 04.advanced-components/src/App.tsx
@@ -1,4 +1,6 @@
 import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
 
 const App = () => {
   return (
@@ -6,6 +8,21 @@ const App = () => {
       <Input label='your name' id='username' type='text' />
       <Input label='your email' id='email' type='text' />
       <Input label='your age' id='age' type='number' />
+
+      <Container
+        className='button'
+        as={Button}
+        onClick={() => console.log('Hello')}
+      >
+        Click me
+      </Container>
+
+      <p>
+        <Button href='https://www.youtube.com/watch?v=UvNxK_Rtdy4'>Link</Button>
+      </p>
+      <p>
+        <Button>Button</Button>
+      </p>
     </main>
   )
 }
---
+++ 04.advanced-components/src/components/Container.tsx
@@ -0,0 +1,22 @@
+import {
+  type ComponentPropsWithoutRef,
+  type ElementType,
+  type ReactNode,
+} from 'react'
+
+type ContainerProps<T extends ElementType> = {
+  as?: T
+  children: ReactNode
+} & ComponentPropsWithoutRef<T>
+
+const Container = <U extends ElementType>({
+  as,
+  children,
+  ...props
+}: ContainerProps<U>) => {
+  const Component = as || 'div'
+
+  return <Component {...props}>{children}</Component>
+}
+
+export default Container
