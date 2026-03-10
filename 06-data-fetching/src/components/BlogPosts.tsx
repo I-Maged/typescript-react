4e102f6012d4763fe5b2d560c5f6c0ebcc120df5
I-Maged <magedhanafy@gmail.com>
2026-03-10 17:21:23 +0200

    starting project

---
+++ 06-data-fetching/src/components/BlogPosts.tsx
@@ -0,0 +1,23 @@
+import type { FC } from 'react'
+
+export type BlogPost = { id: number; title: string; text: string }
+
+type BlogPostsProps = { posts: BlogPost[] }
+
+const BlogPosts: FC<BlogPostsProps> = ({ posts }) => {
+  return (
+    <div id='blog-posts'>
+      <h1>Blog Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.text}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default BlogPosts
