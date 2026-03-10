40006f2376d5f176fe0b390ab226630161aaabd9
I-Maged <magedhanafy@gmail.com>
2026-03-10 10:17:41 +0200

    effect & ref

--- 05-Context-Reducer/src/components/Timer.tsx
+++ 05-Context-Reducer/src/components/Timer.tsx
@@ -1,12 +1,50 @@
-import type { FC } from 'react'
-import { type Timer as TimerProps } from '../context/timers-context'
+import { useEffect, useRef, useState, type FC } from 'react'
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from '../context/timers-context'
 import Container from './ui/Container'
 
 const Timer: FC<TimerProps> = ({ name, duration }) => {
+  const { isRunning } = useTimersContext()
+  const [remainingTime, setRemainingTime] = useState(duration * 1000)
+
+  const interval = useRef<number | null>(null)
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current)
+  }
+
+  useEffect(() => {
+    let timer: number
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime
+          }
+          return prevTime - 50
+        })
+      }, 50)
+
+      interval.current = timer
+    } else if (interval.current) {
+      clearInterval(interval.current)
+    }
+
+    return () => clearInterval(timer)
+  }, [isRunning])
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   )
 }
