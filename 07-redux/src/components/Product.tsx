68f8b0b498766deb88644fe9cb1bdbb078a2a649
I-Maged <magedhanafy@gmail.com>
2026-03-11 01:31:13 +0200

    store & reducers

--- 07-redux/src/components/Product.tsx
+++ 07-redux/src/components/Product.tsx
@@ -1,10 +1,10 @@
 type ProductProps = {
-  id: string;
-  image: string;
-  title: string;
-  price: number;
-  description: string;
-};
+  id: string
+  image: string
+  title: string
+  price: number
+  description: string
+}
 
 export default function Product({
   image,
@@ -15,18 +15,18 @@ export default function Product({
   function handleAddToCart() {}
 
   return (
-    <article className="product">
+    <article className='product'>
       <img src={image} alt={title} />
-      <div className="product-content">
+      <div className='product-content'>
         <div>
           <h3>{title}</h3>
-          <p className="product-price">${price}</p>
+          <p className='product-price'>${price}</p>
           <p>{description}</p>
         </div>
-        <p className="product-actions">
+        <p className='product-actions'>
           <button onClick={handleAddToCart}>Add to Cart</button>
         </p>
       </div>
     </article>
-  );
+  )
 }
---
+++ 07-redux/src/store/store.ts
@@ -0,0 +1,4 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { cartSlice } from './cartSlice'
+
+export const store = configureStore({ reducer: { cart: cartSlice.reducer } })
